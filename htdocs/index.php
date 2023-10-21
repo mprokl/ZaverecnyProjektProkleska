002632e91d0549f4edddd11092941278027f3215
mprokl <148258663+mprokl@users.noreply.github.com>
2023-10-21 12:52:16 +0200

    Update index.php
    some comments added

--- htdocs/index.php
+++ htdocs/index.php
@@ -7,7 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $surname = $_POST['Surname'];      
     $age = $_POST['age'];
     $phoneNumber = $_POST['phone_number'];
-
+    // insert data into the database //
     insertCustomer($name, $surname, $age, $phoneNumber);
     // refreshing the page after data insertion //
     header("Location: index.php");
