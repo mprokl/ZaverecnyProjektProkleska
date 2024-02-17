945b16122e32b31629b33bb718b4a1076a742997
mprokl <148258663+mprokl@users.noreply.github.com>
2024-02-17 10:59:30 +0100

    Update index.php
    some comments added...

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,13 +4,13 @@ require_once 'form.php';
 require_once 'data_display.php';
     // get the data from the form after submission //
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name = htmlspecialchars($_POST['name']);
-    $surname = htmlspecialchars($_POST['surname']);
-    $age = htmlspecialchars($_POST['age']);
-    $phoneNumber = htmlspecialchars($_POST['phone_number']);
+    $name = htmlspecialchars($_POST['name']); // request for name //
+    $surname = htmlspecialchars($_POST['surname']); // request for surname //
+    $age = htmlspecialchars($_POST['age']); // request for age //
+    $phoneNumber = htmlspecialchars($_POST['phone_number']); // request for phone number //
     // insert data into the database //
     insertCustomer($name, $surname, $age, $phoneNumber);
-    // refreshing the page after data insertion //
+    // refresh page after data insertion //
     header("Location: index.php");
     exit;
 }
